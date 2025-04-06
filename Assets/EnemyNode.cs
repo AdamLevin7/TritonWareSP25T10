237354a73ca898881a303e54ada9afe0f3e84e65
arcpro00 <aedantpt@gmail.com>
2025-04-05 22:07:49 -0700

    Finished Movement Of One Enemy Along Nodes
    Enemy Can Move Along Nodes Starting From Node of Index zero to the Final Node and then stops. Does not handle Enemy Deletion yet

---
+++ Assets/EnemyNode.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class EnemyNode : MonoBehaviour
+{
+    public int NodeIndex = 0; 
+    public Vector3 Velocity;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -3,22 +3,68 @@ using System.Collections.Generic;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    //Enemy Specific References Paired by Index
     public GameObject EnemyPrefab;
     public List<GameObject> EnemyList = new List<GameObject>();
+    public List<Vector3> VelocityList = new List<Vector3>();
+    public List<int> CurrentNodeList = new List<int>();
+    public float EnemySpeed;
+    //References for Creating Node Path
+    public Transform NodeContainer;
+    public List<Vector3> NodePositionList = new List<Vector3>();
+    
+    void UpdateNewVelocity(GameObject enemy,int idx)
+    {
+        Vector3 velocity = (NodePositionList[CurrentNodeList[idx] + 1] - NodePositionList[CurrentNodeList[idx]]).normalized;
+        VelocityList[idx] = velocity;
+    }
+
     void Start()
     {
-        GameObject newEnemy = Instantiate(EnemyPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        // This should be done on load of the scene with the path
+        foreach (Transform child in NodeContainer)
+        {
+            NodePositionList.Add(child.position);
+        }
+
+        // This should be done on event trigger for a new Enemy
+        GameObject newEnemy = Instantiate(EnemyPrefab, NodePositionList[0], Quaternion.identity);
+        VelocityList.Add(new Vector3(0,0,0));
+        CurrentNodeList.Add(0);
         EnemyList.Add(newEnemy);
     }
 
     // Update is called once per frame
     void Update()
     {
+        for (int idx = 0; idx < EnemyList.Count; idx++)
+        {
+                
+            int nodeIndex = CurrentNodeList[idx];
+
+            if (nodeIndex + 1 < NodePositionList.Count)
+            {
+                UpdateNewVelocity(EnemyList[idx], idx);
+                EnemyList[idx].transform.position += EnemySpeed * VelocityList[idx] * Time.deltaTime;
+
+                float DistanceToNode = Vector3.Distance(EnemyList[idx].transform.position, NodePositionList[nodeIndex + 1]);
+                float reachRadius = 1f;
+
+                if (DistanceToNode < reachRadius)
+                {
+                    EnemyList[idx].transform.position = NodePositionList[nodeIndex+1];
+                    CurrentNodeList[idx]++;
+                }
+            }
+        }
+
+
         if (Input.GetKey(KeyCode.J))
         { 
+
             for (int idx = 0; idx < EnemyList.Count; idx++)
             {
-                EnemyList[idx].transform.position = new Vector3(5, 0, 0);
+                EnemyList[idx].transform.position += new Vector3(5, 0, 0);
             }
         }
     }
