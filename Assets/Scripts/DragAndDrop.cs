d2fd18686192e249027e8e0097d64831d101f1ff
AdamLevin7 <adamarilevin@gmail.com>
2025-04-05 20:57:57 -0700

    Pushed tower sprite

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -5,7 +5,9 @@ using UnityEngine.EventSystems;
 
 public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    [SerializeField] private Canvas canvas;
     private RectTransform rectTransform;
+    private GameObject tower;
     private void Awake(){
         rectTransform = GetComponent<RectTransform>();
     }
@@ -14,10 +16,12 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler
     }
     public void OnDrag(PointerEventData eventData){
         Debug.Log("OnDrag");
-        rectTransform.anchoredPosition += eventData.delta;
+        rectTransform.anchoredPosition += eventData.delta  / canvas.scaleFactor;
     }
     public void OnEndDrag(PointerEventData eventData){
         Debug.Log("OnEndDrag");
+        Tower towerToBuild = BuildManager.main.GetSelectedTower();
+        tower = Instantiate(towerToBuild.prefab, transform.position, Quaternion.identity);
     }
     public void OnPointerDown(PointerEventData eventData){
         Debug.Log("OnPointerDown");
