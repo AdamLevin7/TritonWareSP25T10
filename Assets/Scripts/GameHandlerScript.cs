1cc0af6ab0b442f93aba34e682fc1377be2aee3d
aiden3918 <liaiden3918@gmail.com>
2025-04-07 10:55:27 -0700

    lose screen implemented (uitesting)

--- Assets/Scripts/GameHandlerScript.cs
+++ Assets/Scripts/GameHandlerScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,13 +17,20 @@ public class GameHandlerScript : Singleton
 
     public List<GameObject> availableTowers;
 
+    [SerializeField] private GameObject GameplayUI;
+    [SerializeField] private GameObject LoseScreenUI;
+
+    public bool playing;
+    public bool endingGame;
+    [SerializeField] private float endSlowdownRate;
+    [SerializeField] private float endAnimationDuration;
+    private float endAnimationCtr;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         // arbitrary
-        lives = maxLives;
-        currentRound = maxRounds;
-        money = 9999999;
+        BeginNewGame();
+        money = 999999;
     }
 
     // Update is called once per frame
@@ -31,6 +39,55 @@ public class GameHandlerScript : Singleton
         // testing only!
         if (Input.GetKey(KeyCode.L)) lives--;
         if (Input.GetKey(KeyCode.M)) money--;
+
+        if (lives <= 0)
+        {
+            EndGame();
+        }
+
+        /*
+        if (endingGame)
+        {
+            Time.timeScale = Math.Max(0.0f, Time.timeScale - endSlowdownRate * Time.deltaTime);
+            endAnimationCtr += Time.deltaTime;
+            if (endAnimationCtr > endAnimationDuration)
+            {
+                GameplayUI.SetActive(false);
+                LoseScreenUI.SetActive(true);
+                endingGame = false;
+            }  
+        }
+        */
+    }
+
+    // end game and show lose screen (usually when)
+    public void EndGame()
+    {
+        playing = false;
+        endingGame = true;
+
+        GameplayUI.SetActive(false);
+        LoseScreenUI.SetActive(true);
+    }
+
+    // begins new game (used in callbacks and button triggers)
+    public void BeginNewGame()
+    {
+        playing = true;
+        currentRound = 1;
+        lives = maxLives;
+        money = 0;
+        GameplayUI.SetActive(true);
+        LoseScreenUI.SetActive(false);
+
+        endAnimationCtr = 0.0f;
+        endingGame = false;
+    }
+
+    // function to go back to menu (mainly used for buttons)
+    public void GoToMenu()
+    {
+        Debug.Log("pretend this button works right now");
     }
 
     // template function for checking whether or not there are any bonuses to be applied due to balances
--- Assets/Scripts/UIHandlerScript.cs
+++ Assets/Scripts/UIHandlerScript.cs
@@ -1,19 +1,23 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIHandlerScript : Singleton
 {
     [SerializeField] private GameObject canvas;
+    public GameObject roundActiveComponent;
     [SerializeField] private GameObject livesText;
     [SerializeField] private GameObject moneyText;
     [SerializeField] private GameObject roundWaveText;
+    public GameObject roundLossComponent;
+    [SerializeField] private GameObject restartButton;
 
     [SerializeField] private GameObject gameManager;
 
     private Text livesTextComponent;
     private Text moneyTextComponent;
     private Text roundWaveTextComponent;
-    private GameHandlerScript gameManagerClass;
+    private GameHandlerScript gameManagerClass; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
@@ -27,6 +31,8 @@ public class UIHandlerScript : Singleton
     // Update is called once per frame
     void Update()
     {
+        if (!gameObject.activeSelf) return;
+
         livesTextComponent.text = "Lives: " + gameManagerClass.lives;
         moneyTextComponent.text = "Money: " + gameManagerClass.money;
         roundWaveTextComponent.text = "Wave: " + gameManagerClass.currentRound + "/" + gameManagerClass.maxRounds;
