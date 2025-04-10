8d30c6e095325bb1788d73657f133966edfd26a4
AdamLevin7 <adamarilevin@gmail.com>
2025-04-10 15:00:02 -0700

    Synergy might work but not tested much

--- Assets/Scripts/Synergy.cs
+++ Assets/Scripts/Synergy.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Synergy : MonoBehaviour
 {
@@ -7,8 +8,22 @@ public class Synergy : MonoBehaviour
     private int blueTowers;
     private int yellowTowers;
     private int greenTowers;
+    private bool rbSynergy;
+    private bool rgSynergy;
+    private bool bgSynergy;
+    private bool totalSynergy;
+    [SerializeField] public Image rbSyn;
+    [SerializeField] public Image rgSyn;
+    [SerializeField] public Image bgSyn;
+    [SerializeField] public Image totalSyn;
 
     // Update is called once per frame
+    private void Awake(){
+        rbSyn.enabled = false;
+        rgSyn.enabled = false;
+        bgSyn.enabled = false;
+        totalSyn.enabled = false;
+    }
     public void UpdateTowerSynergy(string type)
     {
         if(type == "Red"){
@@ -23,11 +38,49 @@ public class Synergy : MonoBehaviour
         if(type == "Green"){
             greenTowers++;
         }
+        CalculateSynergies();
     }
-    /*void Update(){
-        Debug.Log("red " + redTowers);
-        Debug.Log("blue " + blueTowers);
-        Debug.Log("yellow " + yellowTowers);
-        Debug.Log("green " + greenTowers);
-    } */
+    void CalculateSynergies(){
+        if(0.9*redTowers < blueTowers && 1.1*redTowers>blueTowers){
+            rbSynergy = true;
+            rbSyn.enabled = true;
+        }
+        else{
+            rbSynergy = false;
+            rbSyn.enabled = false;
+        }
+        
+        if(0.9*redTowers < greenTowers && 1.1*redTowers>greenTowers){
+            rgSynergy = true;
+            rgSyn.enabled = true;
+        }
+        else{
+            rgSynergy = false;
+            rgSyn.enabled = false;
+        }
+
+        if(0.9*blueTowers < greenTowers && 1.1*greenTowers>greenTowers){
+            bgSynergy = true;
+            bgSyn.enabled = true;
+        }
+        else{
+            bgSynergy = false;
+            bgSyn.enabled = false;
+        }
+
+        if(rbSynergy && rgSynergy){
+            totalSynergy = true;
+            totalSyn.enabled = true;
+        }
+        else{
+            totalSynergy = false; 
+            totalSyn.enabled = false;
+        }
+    }
+    void Update(){
+        Debug.Log("rb " + rbSynergy + " r " + redTowers);
+        Debug.Log("rg " + rgSynergy + " b " + blueTowers);
+        Debug.Log("bg " + bgSynergy + " g " + greenTowers);
+        Debug.Log("total " + totalSynergy);
+    } 
 }
