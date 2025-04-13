e2c8e9d0b097eada339c2521109f6736e3e12736
aiden3918 <liaiden3918@gmail.com>
2025-04-13 00:18:59 -0700

    setup for wave manager

---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+
+    public static WaveManager Instance { get; private set; } 
+    public List<WavePattern> possibleWavePatterns;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this) Destroy(this);
+        else Instance = this;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SummonWave(int waveNumber)
+    {
+        switch (waveNumber)
+        {
+            case 1: 
+                // EnemyManager.Instance.AddWavePattern(possibleWavePatterns[69])
+                // EnemyManager.Instance.AddWavePattern(possibleWavePatterns[420])
+                break;
+            case 2: break;
+            case 3: break;
+            default: 
+                // enemy stat changes here
+                // global stat multiplier in EnemyManager?
+                // like hp and speed multis
+                // i.e. scaledHP = 1 + 0.1(waveNumber - GameManager.Instance.maxWaves), scaledSpeed = 1 + 0.1(waveNumber - GameManager.Instance.maxWaves), 
+                break;
+        }
+    }
+}
--- Assets/Scripts/WavePattern.cs
+++ Assets/Scripts/WavePattern.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "WavePattern", menuName = "ScriptableObjects/WavePattern")]
 public class WavePattern: ScriptableObject
 {
-    public Enemy enemyToSummon; // enemy to Summon
+    public GameObject enemyToSummon; // enemy to Summon
     public int numberToSummon;
     public float summonInterval; // seconds
     public float initialDelay; // initial delay before starting summoning in seconds
