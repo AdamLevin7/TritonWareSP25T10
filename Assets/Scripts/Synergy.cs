534fa411564f05c42a577956a7aa94fac9494e36
AdamLevin7 <adamarilevin@gmail.com>
2025-04-13 17:21:31 -0700

    Resonance Bar added

--- Assets/Scripts/Synergy.cs
+++ Assets/Scripts/Synergy.cs
@@ -6,43 +6,46 @@ public class Synergy : MonoBehaviour
 {
     private int redTowers;
     private int blueTowers;
-    private int yellowTowers;
     private int greenTowers;
+    private int totalTowers;
     private bool rbSynergy;
     private bool rgSynergy;
     private bool bgSynergy;
     private bool totalSynergy;
-    public Image rbSyn;
-    public Image rgSyn;
-    public Image bgSyn;
-    public Image totalSyn;
+    //public Image rbSyn;
+    //public Image rgSyn;
+    //public Image bgSyn;
+    public Image redSyn;
+    public Image greenSyn;
+    public Image blueSyn;
+    //public Image totalSyn;
 
     // Update is called once per frame
-    private void Awake(){
+    /*private void Awake(){
         
         rbSyn.enabled = false;
         rgSyn.enabled = false;
         bgSyn.enabled = false;
         totalSyn.enabled = false;
-    }
+    } */
     public void UpdateTowerSynergy(string type, int towersAdded)
     {
         if(type == "Red"){
             redTowers += towersAdded;
+            totalTowers++;
         }
         if(type == "Blue"){
             blueTowers += towersAdded;
-        }
-        if(type == "Yellow"){
-            yellowTowers += towersAdded;
+            totalTowers++;
         }
         if(type == "Green"){
             greenTowers += towersAdded;
+            totalTowers++;
         }
         CalculateSynergies();
     }
     public void CalculateSynergies(){
-        if(0.9*redTowers < blueTowers && 1.1*redTowers>blueTowers){
+        /*if(0.9*redTowers < blueTowers && 1.1*redTowers>blueTowers){
             rbSynergy = true;
             rbSyn.enabled = true;
         }
@@ -77,14 +80,45 @@ public class Synergy : MonoBehaviour
             totalSynergy = false; 
             totalSyn.enabled = false;
         }
+        */
+        if(0.9*redTowers < blueTowers && 1.1*redTowers>blueTowers){
+            rbSynergy = true;
+        }
+        else{
+            rbSynergy = false;
+        }
+        
+        if(0.9*redTowers < greenTowers && 1.1*redTowers>greenTowers){
+            rgSynergy = true;
+        }
+        else{
+            rgSynergy = false;
+        }
+
+        if(0.9*blueTowers < greenTowers && 1.1*blueTowers>greenTowers){
+            bgSynergy = true;
+        }
+        else{
+            bgSynergy = false;
+        }
+
+        if(rbSynergy && rgSynergy){
+            totalSynergy = true;
+        }
+        else{
+            totalSynergy = false; 
+        }
+        redSyn.GetComponent<LayoutElement>().flexibleWidth = (float)redTowers/totalTowers;
+        greenSyn.GetComponent<LayoutElement>().flexibleWidth = (float)greenTowers/totalTowers;
+        blueSyn.GetComponent<LayoutElement>().flexibleWidth = (float)blueTowers/totalTowers;
     }
-   /*
+   
     void Update(){
         
         Debug.Log("rb " + rbSynergy + " r " + redTowers);
         Debug.Log("rg " + rgSynergy + " b " + blueTowers);
         Debug.Log("bg " + bgSynergy + " g " + greenTowers);
-        Debug.Log("total " + totalSynergy);
+        Debug.Log("total " + totalSynergy + " t " + totalTowers);
         
-    } */
+    } 
 }
