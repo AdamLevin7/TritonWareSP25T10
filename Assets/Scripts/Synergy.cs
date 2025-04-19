52ac48ca769b3d105100ea14fd4f1dc6864035a9
AdamLevin7 <adamarilevin@gmail.com>
2025-04-18 21:57:50 -0700

    Text showing number of each tower

---
+++ Assets/Scripts/Synergy.cs
@@ -0,0 +1,166 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Synergy : MonoBehaviour
+{
+    public int redTowers;
+    public int blueTowers;
+    public int greenTowers;
+    private int totalTowers;
+    public bool rbSynergy;
+    public bool rgSynergy;
+    public bool bgSynergy;
+    public bool totalSynergy;
+    //public Image rbSyn;
+    //public Image rgSyn;
+    //public Image bgSyn;
+    public Image redSyn;
+    public Image greenSyn;
+    public Image blueSyn;
+    //public Image totalSyn;
+
+    public float globalDamageScaleFactor;
+    public float globalRangeScaleFactor;
+    public float globalFirerateScaleFactor;
+    public float globalMoneyScaleFactor;
+
+    // Update is called once per frame
+    /*private void Awake(){
+        
+        rbSyn.enabled = false;
+        rgSyn.enabled = false;
+        bgSyn.enabled = false;
+        totalSyn.enabled = false;
+    } */
+    public void UpdateTowerSynergy(string type, int towersAdded)
+    {
+        if(type == "Red"){
+            redTowers += towersAdded;
+            totalTowers++;
+        }
+        if(type == "Blue"){
+            blueTowers += towersAdded;
+            totalTowers++;
+        }
+        if(type == "Green"){
+            greenTowers += towersAdded;
+            totalTowers++;
+        }
+        CalculateSynergies();
+        UIHandlerScript.Instance.UpdateTowerNumbers();
+    }
+    public void CalculateSynergies(){
+        /*if(0.9*redTowers < blueTowers && 1.1*redTowers>blueTowers){
+            rbSynergy = true;
+            rbSyn.enabled = true;
+        }
+        else{
+            rbSynergy = false;
+            rbSyn.enabled = false;
+        }
+        
+        if(0.9*redTowers < greenTowers && 1.1*redTowers>greenTowers){
+            rgSynergy = true;
+            rgSyn.enabled = true;
+        }
+        else{
+            rgSynergy = false;
+            rgSyn.enabled = false;
+        }
+
+        if(0.9*blueTowers < greenTowers && 1.1*greenTowers>greenTowers){
+            bgSynergy = true;
+            bgSyn.enabled = true;
+        }
+        else{
+            bgSynergy = false;
+            bgSyn.enabled = false;
+        }
+
+        if(rbSynergy && rgSynergy){
+            totalSynergy = true;
+            totalSyn.enabled = true;
+        }
+        else{
+            totalSynergy = false; 
+            totalSyn.enabled = false;
+        }
+        */
+        if(0.9*redTowers < blueTowers && 1.1*redTowers>blueTowers){
+            rbSynergy = true;
+        }
+        else{
+            rbSynergy = false;
+        }
+        
+        if(0.9*redTowers < greenTowers && 1.1*redTowers>greenTowers){
+            rgSynergy = true;
+        }
+        else{
+            rgSynergy = false;
+        }
+
+        if(0.9*blueTowers < greenTowers && 1.1*blueTowers>greenTowers){
+            bgSynergy = true;
+        }
+        else{
+            bgSynergy = false;
+        }
+
+        if(rbSynergy && rgSynergy){
+            totalSynergy = true;
+        }
+        else{
+            totalSynergy = false; 
+        }
+        redSyn.GetComponent<LayoutElement>().flexibleWidth = (float)redTowers/totalTowers;
+        greenSyn.GetComponent<LayoutElement>().flexibleWidth = (float)greenTowers/totalTowers;
+        blueSyn.GetComponent<LayoutElement>().flexibleWidth = (float)blueTowers/totalTowers;
+
+        OrderResonanceBar();
+    }
+
+    public void OrderResonanceBar(){
+        if(redTowers >= greenTowers && greenTowers >= blueTowers && redTowers >= blueTowers){
+            redSyn.transform.SetSiblingIndex(0);
+            greenSyn.transform.SetSiblingIndex(1);
+            blueSyn.transform.SetSiblingIndex(2);
+        }
+        else if(redTowers >= blueTowers && blueTowers >= greenTowers && redTowers >= greenTowers){
+            redSyn.transform.SetSiblingIndex(0);
+            greenSyn.transform.SetSiblingIndex(2);
+            blueSyn.transform.SetSiblingIndex(1);
+        }
+        else if(greenTowers >= redTowers && redTowers >= blueTowers && greenTowers >= blueTowers){
+            redSyn.transform.SetSiblingIndex(1);
+            greenSyn.transform.SetSiblingIndex(0);
+            blueSyn.transform.SetSiblingIndex(2);
+        }
+        else if(greenTowers >= blueTowers && blueTowers >= redTowers && greenTowers >= redTowers){
+            redSyn.transform.SetSiblingIndex(2);
+            greenSyn.transform.SetSiblingIndex(0);
+            blueSyn.transform.SetSiblingIndex(1);
+        }
+        else if(blueTowers >= redTowers && redTowers >= greenTowers && blueTowers >= greenTowers){
+            redSyn.transform.SetSiblingIndex(1);
+            greenSyn.transform.SetSiblingIndex(2);
+            blueSyn.transform.SetSiblingIndex(0);
+        }
+        else if(blueTowers >= greenTowers && greenTowers >= redTowers && blueTowers >= redTowers){
+            redSyn.transform.SetSiblingIndex(2);
+            greenSyn.transform.SetSiblingIndex(1);
+            blueSyn.transform.SetSiblingIndex(0);
+        }
+    }
+   /*
+    void Update(){
+        
+        Debug.Log("rb " + rbSynergy + " r " + redTowers);
+        Debug.Log("rg " + rgSynergy + " b " + blueTowers);
+        Debug.Log("bg " + bgSynergy + " g " + greenTowers);
+        Debug.Log("total " + totalSynergy + " t " + totalTowers);
+        
+    } 
+    */
+}
--- Assets/Scripts/UIHandlerScript.cs
+++ Assets/Scripts/UIHandlerScript.cs
@@ -8,7 +8,36 @@ public class UIHandlerScript : Singleton
     [SerializeField] private GameObject moneyText;
     [SerializeField] private GameObject roundWaveText;
 
+<<<<<<< Updated upstream
     [SerializeField] private GameObject gameManager;
+=======
+    [SerializeField] private Sprite waveStartSprite;
+    [SerializeField] private Sprite twoXSpeedSprite;
+    [SerializeField] private Sprite oneXSpeedSprite;
+
+    [Header("Loss UI")]
+    public GameObject roundLossUI;
+    [SerializeField] private GameObject loseRestartBtn;
+    [SerializeField] private GameObject loseMenuBtn;
+
+    [Header("Win UI")]
+    public GameObject roundWinUI;
+    [SerializeField] private GameObject winRestartBtn;
+    [SerializeField] private GameObject winMenuBtn;
+
+    [Header("Tower Selection UI")]
+    [SerializeField] private GameObject towerSelectionUI;
+    [SerializeField] private Text selectedTowerName;
+    [SerializeField] private Text selectedTowerDamage;
+    [SerializeField] private Text selectedTowerValue;
+    [SerializeField] private Image selectedTowerIcon;
+    [SerializeField] private GameObject synergyManager;
+    [SerializeField] private GameObject towerTypes;
+    private GameObject currentSelectedTower;
+    private Tower currentSelectedTowerClass;
+
+    private TowerData currentSelectedTowerData;
+>>>>>>> Stashed changes
 
     private Text livesTextComponent;
     private Text moneyTextComponent;
@@ -31,5 +60,10 @@ public class UIHandlerScript : Singleton
         moneyTextComponent.text = "Money: " + gameManagerClass.money;
         roundWaveTextComponent.text = "Round: " + gameManagerClass.currentRound + "/" + gameManagerClass.maxRounds;
 
+    }
+    public void UpdateTowerNumbers(){
+        towerTypes.GetComponent<TMPro.TextMeshProUGUI>().text = "<color=\"red\">Red Towers: " + synergyManager.GetComponent<Synergy>().redTowers + 
+                                                              "\n<color=\"green\">Green Towers: " + synergyManager.GetComponent<Synergy>().greenTowers + 
+                                                              "\n<color=#03a1fc>Blue Towers: " + synergyManager.GetComponent<Synergy>().blueTowers;
     }
 }
